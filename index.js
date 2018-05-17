b73abe3b55e2b284af41c5dc396854aba2d2bf9c
Rayyan Karim <rkarim0598@gmail.com>
2018-05-16 20:19:52 -0400

    Alert hi upon delete press

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const form = document.querySelector('#mainForm')
+const arrayyan = []
+var counter = 0
+
+function deleteDis(ev) {
+    var index = ev.target.id.replace('button-', '')
+    console.log(index)
+    list.removeChild(list.childNodes[parseInt(index)])
+}
 
 function renderListItem(label, value) {
     const item = document.createElement('li')
-    item.textContent = `${label}: ${value}`
-
+    item.appendChild(document.createTextNode(`${label}: ${value}`))
+    const button = document.createElement('BUTTON')
+    button.innerHTML = `Delete ${counter}`
+    button.setAttribute("id", `button-${counter}`)
+    alert(button.id)
+    item.appendChild(button)
+    item.setAttribute("id", `thing-${counter}`)
+    // button.addEventListener("submit", deleteDis)
+    counter = counter + 1
+    // item.textContent = `${label}: ${value}`
+    arrayyan.push(item)
+    // debugger
+    // const str = `button${counter}`
+    // alert(str)
+    // const bob = document.getElementById(`${button.id}`)
+    // bob.addEventListener("submit", deleteDis)
     return item
 }
 
@@ -25,10 +47,13 @@ const handleSubmit = function(ev) {
     const player = {
         name: f.playerName.value
     }
-
+ 
     const players = document.querySelector('#players')
     players.appendChild(renderList(player))
-
+    const bob = document.getElementById(`button-${counter-1}`)
+    bob.addEventListener("click", function() {
+        alert('hi')
+    })
     f.reset()
     f.playerName.focus()
 }
