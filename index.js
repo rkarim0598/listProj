7f23d37c92c9e7ef77302d7de1a69f1832dc5b6f
Rayyan Karim <rkarim0598@gmail.com>
2018-05-17 14:33:02 -0400

    Now updated to Davey's code

--- index.js
+++ index.js
@@ -1,67 +1,16 @@
-const form = document.querySelector('#mainForm')
-const arrayyan = []
-var counter = 0
-const list = document.createElement('ul')
-
-function parser(ev) {
-    var index = ev.target.id.replace('button-', '')
-    return index
-}
-
-function deleteDis(items, pos) {
-    try {
-        console.log(pos)
-        // items.removeChild(items.childNodes[pos])
-        // arrayyan.splice(arrayyan.indexOf(items.childNodes[pos]))
-        items.removeChild(document.getElementById(`thing-${pos}`))
-        console.log(arrayyan)
-    } catch (TypeError) {
-        // deleteDis(items, pos-1)
-    }
-}
-
-function renderListItem(label, value) {
-    const item = document.createElement('li')
-    item.appendChild(document.createTextNode(`${label}: ${value}`))
-    const button = document.createElement('BUTTON')
-    button.innerHTML = 'Delete'
-    button.setAttribute("id", `button-${value}`)
-    item.appendChild(button)
-    item.setAttribute("id", `thing-${value}`)
-    counter = counter + 1
-
-    arrayyan.push(item)
-
-    return item
-}
-
-function renderList(data) {
-    // list.setAttribute("id", `list-${counter}`)
-    const labels = Object.keys(data)
-    labels.forEach(function(label) {
-        const item = renderListItem(label, data[label])
-        list.appendChild(item)
-    })
-    
-    return list
-}
-
-const handleSubmit = function(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const player = {
-        name: f.playerName.value
-    }
- 
-    const players = document.querySelector('#players')
-    players.appendChild(renderList(player))
-    const bob = document.getElementById(`button-${player.name}`)
-    bob.addEventListener("click", function() {
-        var index = parser(event)
-        deleteDis(list, index)
-    })
-    f.reset()
-    f.playerName.focus()
-}
-
-form.addEventListener('submit', handleSubmit)
+const app = {
+    init: function(formSelector) {
+      document
+        .querySelector(formSelector)
+        .addEventListener('submit', this.handleSubmit)
+    },
+  
+    handleSubmit: function(ev) {
+      ev.preventDefault()
+      const f = ev.target
+      console.log(f.flickName.value)
+    },
+  }
+  
+  app.init('#flickForm')
+  
